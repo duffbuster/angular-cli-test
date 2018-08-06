df0653ac7ff3c9bf714dd76f4e5305deb519603c
Colin Mackey <colin.mackey@udig.com>
2018-08-06 14:31:12 -0400

    Removed unused files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './app.effects';
-import * as fromUser from './user.reducer';
-import { UserEffects } from './user.effects';
-import { SearchboxComponent } from './searchbox/searchbox.component';
-import { FilterPipe } from './filter.pipe';
-import { TestComponent } from './test/test.component';
+import {AppComponent} from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {reducers, metaReducers} from './reducers';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+import {EffectsModule} from '@ngrx/effects';
+import {AppEffects} from './app.effects';
+import {SearchboxComponent} from './searchbox/searchbox.component';
+import {FilterPipe} from './filter.pipe';
+import {TestComponent} from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchboxComponent,
-    FilterPipe,
-    TestComponent
+    AppComponent, SearchboxComponent, FilterPipe, TestComponent
   ],
   imports: [
-    BrowserModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    EffectsModule.forRoot([AppEffects]),
-    StoreModule.forFeature('user', fromUser.reducer),
-    EffectsModule.forFeature([UserEffects])
+    BrowserModule, StoreModule.forRoot(reducers, {metaReducers}),
+    !environment.production
+      ? StoreDevtoolsModule.instrument()
+      : [],
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
